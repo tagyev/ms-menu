05efa227c38939927d0c32841c21c2a145724b21
tagyev <allahverditagyev@gmail.com>
2025-10-04 12:31:18 +0400

    task

---
+++ src/main/java/com/example/msmenu/MsMenuApplication.java
@@ -0,0 +1,15 @@
+package com.example.msmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class MsMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsMenuApplication.class, args);
+    }
+
+}
